ce1107ef50db1b74935f6b97fbfba4e672eb4791
Zetta-tech <84168491+Zetta-tech@users.noreply.github.com>
2021-10-29 23:02:15 +0530

    Update Covid Case Study.sql

--- Covid Case Study.sql
+++ Covid Case Study.sql
@@ -1,32 +1,54 @@
+/*
+	THE LATEST COVID CASES, DEATHS, VACCINATIONS, AND SO ON HAVE BEEN CLEANED UP, AND DATA RELATING TO COVID CASES WAS SAVED AS AN EXCEL FILE, 
+	WHILE DATA RELATING TO VACCINATIONS WAS SAVED IN ANOTHER EXCEL FILE. THE TWO FIIES WERE COMBINED AND ANALYZED.
+
+	CovidDeaths: Information on COVID cases from around the world. 
+	CovidVaccinations: Vaccination data from around the world.
+*/
+
 SELECT *
-FROM FolioProject..CovidDeaths
+FROM FolioProject..CovidDeaths 
 WHERE continent IS NOT NULL
 ORDER BY 3, 4
 
---	SELECT *
---	FROM FolioProject..CovidVaccinations
---	ORDER BY 3, 4
-
-SELECT location, date, total_cases, new_cases, total_deaths, population
+-- TO DIPLAY JUST THE REQUIRED COLUMNS
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	new_cases, 
+	total_deaths, 
+	population
 FROM FolioProject..CovidDeaths
 ORDER BY 1, 2
 
 -- LOOKING AT TOTAL CASES VS TOTAL DEATHS
 -- LIKELIHOOD OF CONTRACTING COVID IN YOUR COUNTRY
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS deaths_percentage
+
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	total_deaths, 
+	(total_deaths/total_cases)*100 AS deaths_percentage
 FROM FolioProject..CovidDeaths
 WHERE location = 'India'
 ORDER BY 1, 2
 
---TOTAL CASES VS THE POPULATION
+-- TOTAL CASES VS THE POPULATION
 -- PERCENTAGE OF POPULATION THAT GOT COVID
-SELECT location, date, total_cases, population, (total_cases/population)*100 AS cases_percentage
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	population, 
+	(total_cases/population)*100 AS cases_percentage
 FROM FolioProject..CovidDeaths
---WHERE location LIKE '%canada%'
-WHERE continent IS NOT NULL
+WHERE continent IS NOT NULL -- REQUIRED TO DISPLAY COUNTRY WISE DATA RATHER THAN CONTINENT WISE
 ORDER BY 1, 2
 
 --COUNTRIES WITH HIGHEST INFECTION RATES COMPARED TO POPULATION
+
 SELECT 
 	location, 
 	MAX(total_cases) AS highest_per_country, 
@@ -34,7 +56,6 @@ SELECT
 	MAX(total_cases/population)*100 AS percent_population_affected
 FROM 
 	FolioProject..CovidDeaths
---WHERE location LIKE '%canada%'
 WHERE continent IS NOT NULL
 GROUP BY 
 	location, 
@@ -42,14 +63,12 @@ GROUP BY
 ORDER BY percent_population_affected DESC
 
 -- COUNTRIES WITH THE HIGHEST DEATH COUNT BY POPULATION
+
 SELECT 
 	location, 
 	MAX(CAST(total_deaths AS INT)) AS Total_death_count
-	--population, 
-	--MAX(total_deaths/population)*100 AS percent_population_dead
 FROM 
 	FolioProject..CovidDeaths
---WHERE location LIKE '%canada%'
 WHERE continent IS NOT NULL
 GROUP BY 
 	location, 
@@ -57,12 +76,12 @@ GROUP BY
 ORDER BY Total_death_count DESC
 
 -- CONTINENTS WITH HIGHEST DEATH COUNT
+
 SELECT 
 	continent, 
 	MAX(CAST(total_deaths AS INT)) AS Total_death_count
 FROM 
 	FolioProject..CovidDeaths
---WHERE location LIKE '%canada%'
 WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY Total_death_count DESC
@@ -70,16 +89,14 @@ ORDER BY Total_death_count DESC
 --GLOBAL NUMBERS
 
 SELECT 
-	--date, 
 	SUM(new_cases) AS total_cases, 
 	SUM(CAST(new_deaths AS INT)) AS total_deaths,  
 	(SUM(CAST(new_deaths AS INT))/SUM(new_cases))*100 AS deaths_percentage
 FROM FolioProject..CovidDeaths
 WHERE continent IS NOT NULL
---GROUP BY date
 ORDER BY 1, 2
 
---LOOKING AT TOTAL POPULATION VS VACCINATION
+--LOOKING AT TOTAL POPULATION VS VACCINATION AND GET A CUMULATIVE COUNT OF VACCINATED PEOPLE BY LOCATION AND DATE
 
 SELECT 
 	dea.continent,
@@ -99,7 +116,8 @@ WHERE
 ORDER BY
 	2, 3
 
--- USING CTE (Common Table Expressions) OR A VIEW
+-- USING CTE (Common Table Expressions)
+
 WITH PopvsVac(Continent, Location, Date, Population, New_Vaccinations, Rolling_People_Vaccinated)
 AS
 (
@@ -119,17 +137,19 @@ AS
 	WHERE 
 		dea.continent IS NOT NULL
 )
-SELECT *, (Rolling_People_Vaccinated/Population)*100 AS percentage_of_people_vaccinated
+SELECT 
+	*, 
+	(Rolling_People_Vaccinated/Population)*100 AS percentage_of_people_vaccinated
 FROM PopvsVac
 
 
 
--- TEMP TABLE 
-DROP TABLE IF EXISTS #PercentPopulationVaccination
+-- TEMP TABLE (SAME FUNCTIONS PERFORMED AS ABOVE)
+DROP TABLE IF EXISTS #PercentPopulationVaccination -- ADD WHEN QUERY IS RUN MULTIPLE TIMES ELSE ERROR: TABLE ALREADY EXISTS
 CREATE TABLE #PercentPopulationVaccination
 (
-	Continent nvarchar(2505),
-	Location nvarchar(2550),
+	Continent nvarchar(255),
+	Location nvarchar(255),
 	Date datetime,
 	Population numeric,
 	New_Vaccinations numeric,
@@ -151,11 +171,12 @@ SELECT
 	ON 
 		dea.location = vac.location AND dea.date = vac.date
 
-SELECT *, (Rolling_People_Vac/Population)*100
+SELECT *, (Rolling_People_Vac/Population)*100 AS percentage_of_people_vaccinated
 FROM #PercentPopulationVaccination
 
 
---CREATING VIEW TO STORE DATA FOR LATER
+-- CREATING A VIEW TO STORE DATA FOR LATER
+
 CREATE VIEW PercentPopulationVaccinated AS
 SELECT 
 	dea.continent,
